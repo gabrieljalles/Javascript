e38f84a349232320a44ae1fce7c844640f74813e
Jalles <81190552+gabrieljalles@users.noreply.github.com>
2022-06-08 15:03:54 -0300

    Add files via upload

--- Small projects/atividade-palindromo/script.js
+++ Small projects/atividade-palindromo/script.js
@@ -1,7 +1,36 @@
-function hePalindromo(frase) {
-    let palavraJ = frase.replace(/\s+/g,"")
-    let palavraInv = frase.replace(/\s+/g,"").split("").reverse().join("")
-    palavraInv == palavraJ ?console.log( `A frase ${frase} é um palindromo`):console.log(`A frase ${frase} NÃO é um palindromo`);
+//primeira solução
+    //ignora espaços
+    // há sensibilidade com acentos.
+function ehPalindromo(frase) {
+    if(!frase){console.log("Não foi passado nenhuma frase!")}
+    else{
+        let palavraJ = frase.replace(/\s+/g,"")
+        let palavraInv = frase.replace(/\s+/g,"").split("").reverse().join("")
+        palavraInv == palavraJ ?console.log( `A frase ${frase} é um palindromo`):console.log(`A frase ${frase} NÃO é um palindromo`);
+    }
 }
-hePalindromo("anotaram a data da maratona")
 
+//ehPalindromo("")
+
+
+
+//Segunda solução
+function ehPalindromo2(frase){
+    if(!frase){console.log("Não foi passado nenhuma frase!")}
+    else{
+        let resultado;
+        let j = frase.length -1
+        for(let i =0; i<frase.length;i++){
+            if(frase[i]==frase[j]){
+                resultado = true;
+                j-=1
+            }else{
+                resultado = false; 
+                break; 
+            }
+        }
+        resultado == true ? console.log(`A frase ${frase} é um palindromo`):console.log(`A frase ${frase} NÃO é um palindromo`)
+    }
+}
+
+ehPalindromo2("aja okvlko aja")
